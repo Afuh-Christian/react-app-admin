886d022721675a4e24d0191060b57999440e8a9c
Afuh Christian <afuhchristian10@gmail.com>
2024-04-25 21:30:11 +0100

    1st commit

---
+++ src/views/base/index.js
@@ -0,0 +1,32 @@
+import Breadcrumbs from './breadcrumbs/Breadcrumbs'
+import Cards from './cards/Cards'
+import Carousels from './carousels/Carousels'
+import Collapses from './collapses/Collapses'
+import Dropdowns from './Dropdowns'
+import Jumbotrons from './Jumbotrons'
+import ListGroups from './ListGroups'
+import Navbars from './Navbars'
+import Navs from './Navs'
+import Paginations from './Paginations'
+import Popovers from './Popovers'
+import ProgressBar from './ProgressBar'
+import Tabs from './Tabs'
+import Tooltips from './Tooltips'
+// import Dropdowns from './../buttons/dropdowns/Dropdowns';
+
+export {
+  Breadcrumbs,
+  Cards,
+  Carousels,
+  Collapses,
+  Dropdowns,
+  Jumbotrons,
+  ListGroups,
+  Navbars,
+  Navs,
+  Popovers,
+  ProgressBar,
+  Tabs,
+  Tooltips,
+  Paginations,
+}
